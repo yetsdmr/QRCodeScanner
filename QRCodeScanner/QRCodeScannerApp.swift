c02ede2809eb5a833a886402e294abe7e8b4d542
Yunus Emre Taşdemir <yetsdmr@gmail.com>
2023-09-12 15:31:08 +0300

    Initial Commit

---
+++ QRCodeScanner/QRCodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRCodeScannerApp.swift
+//  QRCodeScanner
+//
+//  Created by Yunus Emre Taşdemir on 12.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct QRCodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
