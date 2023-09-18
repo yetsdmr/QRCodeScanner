8018012643dbfd15808932c29bc100adc943bb5e
Yunus Emre Taşdemir <yetsdmr@gmail.com>
2023-09-18 16:49:19 +0300

    Building Scanner View.

--- QRCodeScanner/View/ScannerView.swift
+++ QRCodeScanner/View/ScannerView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct ScannerView: View {
+    // QR Code Scanner Properties
+    @State private var isScanning: Bool = false
     var body: some View {
         VStack(spacing: 8) {
             Button {
@@ -31,7 +33,35 @@ struct ScannerView: View {
             Spacer(minLength: 0)
             
             // Scanner
-            
+            GeometryReader {
+                let size = $0.size
+                
+                ZStack {
+                    ForEach(0...4, id: \.self) { index in
+                        let rotation = Double (index) * 90
+                        
+                        RoundedRectangle(cornerRadius: 2, style: .circular)
+                            // Trimming to get Scanner like Edges
+                            .trim(from: 0.61, to: 0.64)
+                            .stroke(.blue, style: StrokeStyle(lineWidth: 5, lineCap: .round, lineJoin: .round))
+                            .rotationEffect(.init(degrees: rotation))
+                    }
+                    
+                }
+                // Sqaure Shape
+                .frame(width: size.width, height: size.width)
+                // Scanner Animation
+                .overlay(alignment: .top, content: {
+                    Rectangle()
+                        .fill(.blue)
+                        .frame(height: 2.5)
+                        .shadow(color: .black.opacity(0.8), radius: 8, x: 0, y: isScanning ? 15 : -15)
+                        .offset(y: isScanning ? size.width : 0)
+                })
+                // To Make it Center
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+            }
+            .padding(.horizontal, 45)
             
             Spacer(minLength: 15)
             
@@ -47,6 +77,18 @@ struct ScannerView: View {
 
         }
         .padding(15)
+        // For UI Demo
+        .onAppear {
+            activateScannerAnimation()
+        }
+    }
+    
+    // Activating Scanner Animation Method
+    func activateScannerAnimation() {
+        // Adding Delay for Each Reversal
+        withAnimation(.easeInOut(duration: 0.85).delay(0.1).repeatForever(autoreverses: true)) {
+            isScanning = true
+        }
     }
 }
 
